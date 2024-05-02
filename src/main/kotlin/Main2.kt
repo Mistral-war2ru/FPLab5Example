7f4a1124d7b62f76d7a5a8c520c4be2ed709aa08
@EduardGorodetzkiy <xxxyyyzzzed@gmail.com>
2024-05-02 20:27:41 +0300

    Lab6 frequency function

--- src/main/kotlin/Main2.kt
+++ src/main/kotlin/Main2.kt
@@ -3,17 +3,13 @@ import java.util.*
 
 val scanner = Scanner(`in`)
 
-fun vvodd(n: Int, m: MutableList<Int>)
-{
-    if (n>0)
-    {
+fun vvodd(n: Int, m: MutableList<Int>) {
+    if (n>0) {
         m.add(scanner.nextInt())
         vvodd(n-1, m)
     }
 }
-
-fun vvod(n: Int):MutableList<Int>
-{
+fun vvod(n: Int):MutableList<Int> {
     val m:MutableList<Int> = mutableListOf()
     vvodd(n, m)
     return m
@@ -27,14 +23,26 @@ fun muld(m: MutableList<Int>): Int = arrayOP(m, m.size, 1) { a, b -> (a * b) }
 fun maxd(m: MutableList<Int>): Int = arrayOP(m, m.size, m[0]) { a, b -> if (a > b) a else b }
 fun mind(m: MutableList<Int>): Int = arrayOP(m, m.size, m[0]) { a, b -> if (a < b) a else b }
 
+fun freq(m: MutableList<Int>): Int
+{
+    if (m.isNotEmpty()) {
+        val map: MutableMap<Int, Int> = mutableMapOf<Int, Int>()
+        for (x in m) map[x] = map.getOrDefault(x, 0) + 1
+        var k: Int = 1
+        var e: Int = map.values.first()
+        for (x in map) if (x.value > k) {
+            k = x.value
+            e = x.key
+        }
+        return e
+    } else return 0
+}
+
 fun main() {
     println("Hello World!")
     val scanner = Scanner(`in`)
     val n: Int = scanner.nextInt()
     val m = vvod(n)
     println(m)
-    println(sumd(m))
-    println(muld(m))
-    println(maxd(m))
-    println(mind(m))
+    println(freq(m))
 }
